83c7487c42c6e98314a8bddc7c4731d6feebd46a
aaaa110778-web <aaaa110778@gmail.com>
2025-09-17 00:08:52 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'screens/home_page.dart';
+import 'screens/players_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _mode = ThemeMode.light;
+
+  void _toggleTheme(bool isDark) {
+    setState(() {
+      _mode = isDark ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TOP1Score',
+      themeMode: _mode,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green, brightness: Brightness.dark),
+        useMaterial3: true,
+      ),
+      routes: {
+        '/': (_) => HomePage(onToggleTheme: _toggleTheme, isDark: _mode == ThemeMode.dark),
+        '/players': (_) => const PlayersPage(),
+      },
+    );
+  }
+}
