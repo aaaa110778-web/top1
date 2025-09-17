ff78d8587566f09220ceb20b56e07d4157e24aa9
aaaa110778-web <aaaa110778@gmail.com>
2025-09-17 03:02:50 +0300

    Update dark_mode_toggle.dart

--- lib/widgets/dark_mode_toggle.dart
+++ lib/widgets/dark_mode_toggle.dart
@@ -2,17 +2,12 @@ import 'package:flutter/material.dart';
 
 class DarkModeToggle extends StatelessWidget {
   final bool isDark;
-  final void Function(bool) onChanged;
+  final ValueChanged<bool> onChanged;
 
   const DarkModeToggle({super.key, required this.isDark, required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const Icon(Icons.dark_mode),
-        Switch(value: isDark, onChanged: onChanged),
-      ],
-    );
+    return Switch(value: isDark, onChanged: onChanged);
   }
 }
